1bce84cc4fc4ea59d2344fd542eaa261b498b506
chcp <chcp@cin.ufpe.br>
2017-06-19 07:41:05 -0300

    Rename function

--- background.js
+++ background.js
@@ -1,6 +1,46 @@
-var checkBox = document.querySelectorAll("input[type='checkbox']");
-for (var i = 0; i < checkBox.length; i++) 
-  {
-    //checkBox[i].addEventListener('click', click);
-    checkBox[i].checked = true;
+
+function checkAll() 
+{
+	var checkBox = document.querySelectorAll("input[type='checkbox']");
+	for (var i = 0; i < checkBox.length; i++) 
+	  {
+	    checkBox[i].checked = true;
+	  }	
+}
+checkAll()
+function AddCheckBox() 
+{
+    // Get Name
+    var studentName = document.getElementById('newName').value
+    
+    // Show the name
+    var newName = document.createElement("textName");
+	var t = document.createTextNode(studentName);
+	newName.appendChild(t);   
+    var breakLine = document.createElement("br");
+
+	// Create CheckBox
+	var newCheckBox = document.createElement("INPUT");
+    newCheckBox.setAttribute("type", "checkbox");   
+    newCheckBox.setAttribute("name", studentName);   
+    
+    var listOfStudents = document.getElementById("ClassRoom");
+    listOfStudents.appendChild(newCheckBox);
+    listOfStudents.appendChild(newName);
+    listOfStudents.appendChild(breakLine);
+}
+
+function checkPersonInTheBox(sel)
+{
+	var studentName = document.getElementById('newName').value;
+	checkPerson(studentName,sel);
+
+}
+function checkPerson(name, sel) 
+{
+  checkboxes = document.getElementsByName(name);
+  for(var i=0, n=checkboxes.length;i<n;i++) {
+    checkboxes[i].checked = sel;
   }
+}
+AddCheckBox()
--- popup.js
+++ popup.js
@@ -11,8 +11,6 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true},
   });
 
 window.onload = function() {
-
- 
   document.getElementById("pagebutton").addEventListener("click", function(){
     var status = document.getElementById("checkS").checked;
     if(status == true){
@@ -21,6 +19,10 @@ window.onload = function() {
     else{
       document.getElementById("checkS").checked = true;      
     }
+  });
+  
+  document.getElementById("Band").addEventListener("click", function(){
+    addBand()
   });
   
 }
@@ -39,14 +41,44 @@ function startBackgroundScript(e) {
   //window.close();
 }
 
+function AddBandCheckBox() 
+{
+    // Get Name
+    var studentName = "Band"
+    
+    // Show the name
+    var newName = document.createElement("textName");
+    var t = document.createTextNode(studentName);
+    newName.appendChild(t);   
+    var breakLine = document.createElement("br");
+
+  // Create CheckBox
+    var newCheckBox = document.createElement("INPUT");
+    newCheckBox.setAttribute("type", "checkbox");   
+    newCheckBox.setAttribute("name", studentName);   
+    
+    var listOfStudents = document.getElementById("ClassRoom");
+    listOfStudents.appendChild(newCheckBox);
+    listOfStudents.appendChild(newName);
+    listOfStudents.appendChild(breakLine);
+}
+
+function addBand(){
+  console.log("BAAAAAAAAAAAAAAAAAAAAAD")
+  chrome.tabs.executeScript(null,AddBandCheckBox())
+}
+
+
 /**
  *  add event listener to a group of buttuns
  */
 document.addEventListener('DOMContentLoaded', function () {
   var startButton = document.querySelectorAll('button');
-  for (var i = 0; i < startButton.length; i++) {
+  for (var i = 0; i < 1/*startButton.length*/; i++) {
     startButton[i].addEventListener('click', startBackgroundScript);
   }
+
+
 });
 
 
