c1cbbe409e9b10023a1f9481a51223a15b4e8b63
chcp <chcp@cin.ufpe.br>
2017-07-04 13:03:27 -0300

    Remove useless code, and change ID of the button

--- popup.js
+++ popup.js
@@ -11,26 +11,29 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true},
   });
 
 
-function addBand(){
-  console.log("BAAAAAAAAAAAAAAAAAAAAAD")
-  chrome.tabs.executeScript(null,AddBandCheckBox())
-}
-
-
 /**
  *  add event listener to a group of buttuns
  */
-
-function httpGet(theUrl)
+var cpfValue = 3;
+function setCpfValue(newCpf)
 {
+  cpfValue = newCpf;
+}
+
+function httpGet(cpf)
+{
+
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "https://ess-20171-presence-server.herokuapp.com/lesson/listByCpf?cpf=3", false ); // false for synchronous request
+    xmlHttp.open( "GET", "https://ess-20171-presence-server.herokuapp.com/lesson/listByCpf?cpf="+cpf, false ); // false for synchronous request
     xmlHttp.send( null );
     console.log( xmlHttp.responseText);
     return xmlHttp.responseText;
 }
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('button').addEventListener('click', httpGet);
+  //console.log(document.querySelector('button'));
+  //document.querySelector('button').addEventListener('click', httpGet(cpfValue));
+  document.getElementById('RequestJson').addEventListener('click', httpGet(cpfValue));
+
 });
 
 
